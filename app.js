8db58f55e67209d2d7548968518023c9e7c26ccd
Aneel Grewal <aneel.grewal@hotmail.com>
2023-04-29 01:06:12 +0200

    nested react elements

--- app.js
+++ app.js
@@ -6,6 +6,20 @@
         "Hello World! printed using React." //pops or childrens that are passes into the h1 tag
         );
 
+        //created nested element structure
+        /**
+         * div id = parent
+         *      div id = child
+         *                  <h1></h1>
+         *      /div
+         * /div
+         * 
+         */
+    
+        const child = React.createElement("div", {id:"child"}, heading);
+        const parent = React.createElement("div", {id:"parent"}, child);
+
+
         const root = ReactDOM.createRoot(document.getElementById("root"));
 
-        root.render(heading);
+        root.render(parent);
