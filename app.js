c930713faec1526d3cad7a4256dd44dce2ce3738
Aneel Grewal <aneel.grewal@hotmail.com>
2023-04-29 00:43:14 +0200

    attributes are added to the element & index.css file added

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 
-        const heading = React.createElement("h1", {}, "Hello World! printed using React.");
+        //heading is an object or react element
+        const heading = React.createElement(
+        "h1", //html tag name
+        {id:"h1", zyz:"abc"}, //h1 tag properties or attributes
+        "Hello World! printed using React." //pops or childrens that are passes into the h1 tag
+        );
 
         const root = ReactDOM.createRoot(document.getElementById("root"));
 
