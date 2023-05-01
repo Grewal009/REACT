f1befbff46c2ef896c465530d81e946f312ad97c
Aneel Grewal <aneel.grewal@hotmail.com>
2023-05-01 23:54:35 +0200

    JSX is converted or transpiled to javascript code by Babel

--- app.js
+++ app.js
@@ -1,52 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-        //heading is an object or react element
-        const heading = React.createElement(
-        "h1", //html tag name
-        {id:"h1", zyz:"abc"}, //h1 tag properties or attributes
-        "Hello World! printed using React." //pops or childrens that are passes into the h1 tag
-        );
-
-        //created nested element structure
-        /**
-         * div id = parent
-         *      div id = child
-         *                  <h1></h1>
-         *      /div
-         *       div id = child2
-         *                  <h1></h1>
-         *      /div
-         * /div
-         * 
-         */
-    
-        const parent = React.createElement(
-                "div",
-                {id:"parent"},
-                [
-                        React.createElement(
-                                "div",
-                                {id:"child1", key:"child 01", xyx:"1"},
-                                React.createElement(
-                                        "h1",
-                                        {},
-                                        "Heading 1 -->"
-                                )
-                        ),
-                        React.createElement(
-                                "div",
-                                {id:"child2",key:"child 02", xyz:"2"},
-                                React.createElement(
-                                        "h1",
-                                        {},
-                                        "Heading 2 -->"
-                                )
-                        )
-                ]
-        );
-
+        const heading = <h1>Learning React 🚀</h1>
+        console.log(heading);
 
         const root = ReactDOM.createRoot(document.getElementById("root"));
 
-        root.render(parent);
+        root.render(heading);
