804e2a41edcc94878e57e9cb4d0d94a69ec1e524
Aneel Grewal <aneel.grewal@hotmail.com>
2023-05-02 09:52:49 +0200

    React Component vs React Component Composition vs React Element

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-        const heading = <h1>Learning React 🚀</h1>
-        console.log(heading);
+const Heading1 = () => <h3>React Functional Component2</h3>;
 
-        const root = ReactDOM.createRoot(document.getElementById("root"));
+const Heading2 = () => {
+  return (
+    <div>
+      <Heading1 />
+      <h2>React Functional Component1</h2>
+    </div>
+  );
+};
 
-        root.render(heading);
+const heading = (
+  <div>
+    <h1>React Element</h1>
+    <Heading2 />
+  </div>
+);
+console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
