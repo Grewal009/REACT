03dfb46a222eea1b23d2fe30bf533192944d9cca
Aneel Grewal <aneel.grewal@hotmail.com>
2023-05-06 22:33:17 +0200

    tailwindCSS installed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,18 @@
-
+import {GiHotMeal} from 'react-icons/gi';
 
 const Header = () => {
     return(
         <div>
+            <div className="h-24 w-full p-2 border bottom-3 border-blue-500 bg-orange-300">
+                <div>
+                    <div>
+                        <GiHotMeal size={70} className='mx-2 text-amber-900'/>
+                    </div>
+
+                </div>
+
+
+            </div>
 
         </div>
     )
