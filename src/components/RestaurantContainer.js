7d20a6ce10e160199d959d89200f233877e60d30
Aneel Grewal <aneel.grewal@hotmail.com>
2023-05-09 18:29:15 +0200

    filter restaurants based on rating i.e 4+

--- src/components/RestaurantContainer.js
+++ src/components/RestaurantContainer.js
@@ -1,18 +1,54 @@
+import React, { useState } from 'react';
 import RestaurantCard from "./RestaurantCard";
 import { restList } from "../utils/mockData";
-import {useState} from 'react';
+
 
 const RestaurantContainer = () => {
-    //const {restaurants, setRestaurants} = useState(restList);
+    
     console.log(restList);
 
+    const [rest, setRest] = useState(restList);
+    const [showRating, setShowRating] = useState(true);
+
+    function allRestaurants(){
+        setRest(restList);
+        setShowRating(!showRating);
+    }
+
+    function fourPlusRating(){
+     const fourPlusRatingRestaurants = restList.filter(
+            (res)=> res.data.avgRating > 4
+        );
+    console.log(fourPlusRatingRestaurants);
+    setRest(fourPlusRatingRestaurants);
+    setShowRating(!showRating);
+    }
+
     return(
-        <div className="flex flex-wrap justify-center">
-            {
-                restList.map((restaurant)=>(<RestaurantCard key={restaurant.data.id} restData={restaurant}/>))
-            }
-            
-        </div>
+
+        <React.Fragment>
+            <div>
+                {
+                    (showRating)
+                    ?(<button className='w-40 px-3 py-1 bg-yellow-600 text-yellow-100 rounded-md hover:bg-yellow-100 hover:text-yellow-600 border-4 border-transparent hover:border-4 hover:border-yellow-600'
+                    onClick={fourPlusRating}
+                    >4+ rating</button>)
+                    :(<button className='w-40 px-3 py-1 bg-yellow-600 text-yellow-100 rounded-md hover:bg-yellow-100 hover:text-yellow-600 border-4 border-transparent hover:border-4 hover:border-yellow-600'
+                    onClick={allRestaurants}
+                    >All Restaurant</button>)
+                }
+                
+
+                
+
+            </div>
+            <div className="flex flex-wrap justify-center">
+                {
+                    rest.map((restaurant)=>(<RestaurantCard key={restaurant.data.id} restData={restaurant}/>))
+                }
+                
+            </div>
+        </React.Fragment>
     )
 }
 
