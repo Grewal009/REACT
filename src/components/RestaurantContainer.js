158fbe32ca7082f861cdcc38bfceeb645a0977e6
Aneel Grewal <aneel.grewal@hotmail.com>
2023-05-14 00:32:49 +0200

    search button added

--- src/components/RestaurantContainer.js
+++ src/components/RestaurantContainer.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import RestaurantCard from "./RestaurantCard";
 import { restList } from "../utils/mockData";
+import {AiOutlineSearch} from 'react-icons/ai';
 
 
 const RestaurantContainer = () => {
@@ -9,6 +10,7 @@ const RestaurantContainer = () => {
 
     const [rest, setRest] = useState(restList);
     const [showRating, setShowRating] = useState(true);
+    const [searchText, setSearchText] = useState("");
 
     function allRestaurants(){
         setRest(restList);
@@ -27,21 +29,29 @@ const RestaurantContainer = () => {
     return(
 
         <React.Fragment>
-            <div>
-                {
-                    (showRating)
-                    ?(<button className='w-40 px-3 py-1 bg-yellow-600 text-yellow-100 rounded-md hover:bg-yellow-100 hover:text-yellow-600 border-4 border-transparent hover:border-4 hover:border-yellow-600'
-                    onClick={fourPlusRating}
-                    >4+ rating</button>)
-                    :(<button className='w-40 px-3 py-1 bg-yellow-600 text-yellow-100 rounded-md hover:bg-yellow-100 hover:text-yellow-600 border-4 border-transparent hover:border-4 hover:border-yellow-600'
-                    onClick={allRestaurants}
-                    >All Restaurant</button>)
-                }
-                
+            <div className='flex justify-center'>
+                <div>
+                    <input type='text' name='searchTxt' value={searchText} onChange={(e)=>{setSearchText(e.target.value)}} className='w-48 h-10 bg-slate-200 px-2 py-1 border-transparent border-gray-500 rounded-tl-2xl rounded-bl-2xl rounded-tr-none rounded-t
+                    br-none text-lg font-medium focus:rounded-tl-2xl focus:rounded-bl-2xl focus:rounded-tr-none focus:rounded-br-none border-r-0' />
 
-                
+                    <button  className='w-20 h-10  border-1 border-transparent bg rounded-tr-2xl rounded-br-2xl text-lg font-medium bg-slate-300 hover:bg-slate-400'>Search</button>
 
+                </div>
+
+                <div className='ml-5'>
+                    {
+                        (showRating)
+                        ?(<button className='w-36 px-3 py-1 bg-yellow-600 text-yellow-100 rounded-md hover:bg-yellow-100 hover:text-yellow-600 border-4 border-transparent hover:border-4 hover:border-yellow-600'
+                        onClick={fourPlusRating}
+                        >4+ rating</button>)
+                        :(<button className='w-36 px-3 py-1 bg-yellow-600 text-yellow-100 rounded-md hover:bg-yellow-100 hover:text-yellow-600 border-4 border-transparent hover:border-4 hover:border-yellow-600'
+                        onClick={allRestaurants}
+                        >All Restaurant</button>)
+                    }
+
+                </div>
             </div>
+
             <div className="flex flex-wrap justify-center">
                 {
                     rest.map((restaurant)=>(<RestaurantCard key={restaurant.data.id} restData={restaurant}/>))
