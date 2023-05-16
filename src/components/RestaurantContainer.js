f21b47dc34973215113814ad3ce8a059e75482a1
Aneel Grewal <aneel.grewal@hotmail.com>
2023-05-16 12:13:41 +0200

    fetch data from localhost:7200

--- src/components/RestaurantContainer.js
+++ src/components/RestaurantContainer.js
@@ -32,7 +32,9 @@ const RestaurantContainer = () => {
     async function getRestaurants(){
         //const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.7016176&lng=76.820049&page_type=DESKTOP_WEB_LISTING");
 
-        const data = await fetch("https://foodapp-server-puld.onrender.com/");
+        //const data = await fetch("https://foodapp-server-puld.onrender.com/");
+
+        const data = await fetch("http://localhost:7200/");
 
         const json = await data.json();
         console.log(json);
