be67e11ab338f90301855b2ada58ad9a4ac5fb7f
Aneel Grewal <aneel.grewal@hotmail.com>
2023-05-18 08:15:09 +0200

    early return added

--- src/components/RestaurantContainer.js
+++ src/components/RestaurantContainer.js
@@ -7,7 +7,7 @@ import ShimmerUI from './ShimmerUI';
 
 function filterDate(searchText, allRest){
    return allRest.filter(
-        (restaurant)=>restaurant.data.name.toLowerCase().includes(searchText.toLowerCase())
+        (restaurant)=>restaurant?.data?.name?.toLowerCase().includes(searchText.toLowerCase())
         
     );
 }
@@ -28,13 +28,14 @@ const RestaurantContainer = () => {
 
     const [showRating, setShowRating] = useState(true);
     const [searchText, setSearchText] = useState("");
+    const [prevSearchText, setPrevSearchText] = useState("");
 
     async function getRestaurants(){
-        //const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.7016176&lng=76.820049&page_type=DESKTOP_WEB_LISTING");
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.7016176&lng=76.820049&page_type=DESKTOP_WEB_LISTING");
 
         //const data = await fetch("https://foodapp-server-puld.onrender.com/");
 
-        const data = await fetch("http://localhost:7200/");
+        //const data = await fetch("http://localhost:7200/");
 
         const json = await data.json();
         console.log(json);
@@ -59,16 +60,21 @@ const RestaurantContainer = () => {
     setFilterRest(fourPlusRatingRestaurants);
     setShowRating(!showRating);
     }
+    //early return
+    if(!allRest){
+        return null;
+    }
 
-
-
-    return (filterRest.length === 0) ? (<ShimmerUI />) : (
+    return (allRest.length === 0) ? (<ShimmerUI />) : (
 
         <React.Fragment>
             <div className='flex flex-wrap justify-center'>
                 <div className='my-2'>
-                    <input type='text' name='searchTxt' value={searchText} onChange={(e)=>{setSearchText(e.target.value)}} className='w-48 h-10 bg-slate-200 px-2 py-1 border-transparent border-gray-500 rounded-tl-2xl rounded-bl-2xl rounded-tr-none rounded-t
-                    br-none text-lg font-medium focus:rounded-tl-2xl focus:rounded-bl-2xl focus:rounded-tr-none focus:rounded-br-none border-r-0' />
+                    <input type='text' name='searchTxt' value={searchText} onChange={(e)=>{
+                        setSearchText(e.target.value);
+                        }} 
+                        className='w-48 h-10 bg-slate-200 px-2 py-1 border-transparent border-gray-500 rounded-tl-2xl rounded-bl-2xl rounded-tr-none rounded-t
+                        br-none text-lg font-medium focus:rounded-tl-2xl focus:rounded-bl-2xl focus:rounded-tr-none focus:rounded-br-none border-r-0' />
 
                     <button onClick={
                         ()=>{
@@ -77,6 +83,8 @@ const RestaurantContainer = () => {
                         console.log('allrest', allRest);
                         console.log('searchText',searchText);
                         setFilterRest(data1);
+                        setSearchText("");
+                        setPrevSearchText(searchText);
 
                     }
                     } className='w-20 h-10  border-1 border-transparent bg rounded-tr-2xl rounded-br-2xl text-lg font-medium bg-slate-300 hover:bg-slate-400'>Search</button>
@@ -97,9 +105,19 @@ const RestaurantContainer = () => {
 
                 </div>
             </div>
-
+            {
+            (prevSearchText === "")
+            ? null
+            : <h1 className="flex flex-wrap justify-center">Showing result: {prevSearchText}</h1>
+            }
+            
             <div className="flex flex-wrap justify-center">
-                {
+                
+                {   
+                //if filterRest is zero return no result found.
+                (filterRest.length === 0)
+                    ? <h1>No result found.</h1>
+                    :
                     filterRest.map((restaurant)=>(<RestaurantCard key={restaurant.data.id} restData={restaurant}/>))
                 }
                 
