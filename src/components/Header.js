57ce6ee4dbf505fb7893ec111680a6f89af30e11
Aneel Grewal <aneel.grewal@hotmail.com>
2023-05-18 22:47:08 +0200

    header component edited

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,19 +9,29 @@ const Header = () => {
                 <div className='flex justify-between items-center'>
                     <GiHotMeal size={60} className='mx-2 text-amber-900 bg-green-400'/>
                 
+                <div className='flex justify-start'>
                     <ul className='flex'>
-                        <li className='mx-2 px-2 py-1 text-lg font-bold text-gray-600 hover:text-black'>HOME</li>
-                        <li className='mx-2 px-2 py-1 text-lg font-bold text-gray-600 hover:text-black'>ABOUT</li>
-                        <li className='mx-2 px-2 py-1 text-lg font-bold  text-gray-600 hover:text-black'>CONTACT</li>
+                        <li className='mx-2 py-1 text-lg font-bold text-gray-600 hover:text-black'>HOME</li>
+                        <li className='mx-2  py-1 text-lg font-bold text-gray-600 hover:text-black'>ABOUT</li>
+                        <li className='mx-2 py-1 text-lg font-bold  text-gray-600 hover:text-black'>CONTACT</li>
                         <li></li>
                     </ul>
 
-                    <HiShoppingCart size={35} className='bg-green-500'/>
-                
-                    <div className='flex'>
+                    <HiShoppingCart size={35} className='mx-2 bg-green-500'/>
+                </div>
+
+                    {/** 
+                      <div className='flex'>
                         <button className='px-3 py-2 mx-2 text-lg text-gray-600 hover:text-black font-bold bg-blue-400 rounded-md'>Log in</button>
                         <button className='px-3 py-2 mx-2 text-lg text-gray-600 hover:text-black font-bold bg-blue-400 rounded-md'>Sign up</button>
                     </div>
+                     
+                    */
+                    }
+
+
+
+                   
 
                 </div>
 
