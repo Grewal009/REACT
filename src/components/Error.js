c1265feb8c3923e946bb7b35e5a4eb28fe18f288
Aneel Grewal <aneel.grewal@hotmail.com>
2023-05-18 22:18:08 +0200

    error message generated from useRouteError hook

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,10 +1,17 @@
 import errorImage from '../assets/images/404-error-1.png';
+import  {useRouteError} from 'react-router-dom';
 
 const Error = () => {
+
+    const error = useRouteError();
+    console.log(error);
+    const {status, statusText} = error;
+
     return(
         <div className='w-full h-screen bg-[#C7EBE8] flex flex-col justify-center items-center'>
             <h1 className='text-xl font-bold'>Opps!!!</h1>
             <h2 className='text-lg font-bold'>Something went wrong!</h2>
+            <h3 className='text-lg font-bold'>{status} - {statusText}</h3>
             <img src={errorImage} className='w-80'/>
         </div>
     )
