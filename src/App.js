a06b693fc0c0c0c0d5a8efd2233b925afa9fa386
Aneel Grewal <aneel.grewal@hotmail.com>
2023-05-18 15:02:23 +0200

    router configuration created using createBrowserRouter from react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import About from "./components/About";
 
 const App = () => {
   return(
@@ -15,6 +17,20 @@ const App = () => {
   );
 };
 
+
+const appRouter = createBrowserRouter([
+  {
+    path : '/',
+    element : <App/>,
+  },
+  {
+    path : '/about',
+    element : <About />,
+  }
+]);
+
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render( <RouterProvider router = {appRouter} />);
