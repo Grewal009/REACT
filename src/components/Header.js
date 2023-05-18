402f0175b5e3b84a5a39dd17f7aeeb67a1ba421e
Aneel Grewal <aneel.grewal@hotmail.com>
2023-05-18 23:45:54 +0200

    links are updated in header component using NavLink (react-router-dom)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,16 @@
 import {GiHotMeal} from 'react-icons/gi';
 import {HiShoppingCart} from 'react-icons/hi';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
+    const normalLink = "";
+    const activeLink = "underline underline-offset-4 decoration-lime-600 decoration-4";
+
     return(
         <div>
             <div className="h-20 w-full p-2 bg-orange-300">
 
-                <div className='flex justify-between items-center'>
+                <div className='flex justify-between items-center '>
                     <a href='/'>
                         <GiHotMeal size={60} className='mx-2 text-amber-900 bg-green-400'/>
                     </a>
@@ -14,13 +18,30 @@ const Header = () => {
                 
                 <div className='flex justify-start'>
                     <ul className='flex'>
+                        <NavLink to="/" className={( {isActive} ) =>
+                        isActive ? activeLink : normalLink
+                         }>
                         <li className='mx-2 py-1 text-lg font-bold text-gray-600 hover:text-black'>HOME</li>
+                        </NavLink>
+                        
+                        <NavLink to="/about" className={( {isActive} ) =>
+                        isActive ? activeLink : normalLink
+                         }>
                         <li className='mx-2  py-1 text-lg font-bold text-gray-600 hover:text-black'>ABOUT</li>
-                        <li className='mx-2 py-1 text-lg font-bold  text-gray-600 hover:text-black'>CONTACT</li>
-                        <li></li>
-                    </ul>
+                        </NavLink>
 
+                        <NavLink to="/contact" className={( {isActive} ) =>
+                        isActive ? activeLink : normalLink
+                         }>
+                        <li className='mx-2 py-1 text-lg font-bold  text-gray-600 hover:text-black'>CONTACT</li>
+                        </NavLink>
+
+                    </ul>
+                    <NavLink to="/cart" className={( {isActive} ) =>
+                        isActive ? activeLink : normalLink
+                         }>
                     <HiShoppingCart size={35} className='mx-2 bg-green-500'/>
+                    </NavLink>
                 </div>
 
                     {/** 
