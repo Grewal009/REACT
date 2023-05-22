13db0f15c81fd8e1f60fe2de6db993762b14ab30
Aneel Grewal <aneel.grewal@hotmail.com>
2023-05-22 14:41:28 +0200

    optional chaining added to RestaurantMenu component

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -35,7 +35,7 @@ const RestaurantMenu = () => {
             <div className='text-center'>
             <h1 className='text-sm'>ID:{id}</h1>
 
-            <h2 className='text-4xl font-bold leading-8'>{restaurant?.cards[0]?.card?.card?.info?.name}</h2>
+            <h2 className='text-4xl font-bold leading-8 mb-1'>{restaurant?.cards[0]?.card?.card?.info?.name}</h2>
 
             <h3 className='text-lg font-medium leading-5 mb-1'>{restaurant?.cards[0]?.card?.card?.info?.areaName}, {restaurant?.cards[0]?.card?.card?.info?.city}</h3>
 
@@ -53,21 +53,36 @@ const RestaurantMenu = () => {
 
             <ul>
                 {
-                    restaurant.cards[2].groupedCard.cardGroupMap.REGULAR.cards[1].card.card.itemCards.map(
+                    restaurant?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards?.map(
                         (rest)=>(
                         
-                        <li key={rest.card.info.id} className='p-1  mx-5 my-5 flex flex-col justify-between shadow-md shadow-slate-900'>
+                        <li key={rest?.card?.info?.id} className='p-1  mx-5 my-5 flex flex-col justify-between shadow-md shadow-slate-900'>
 
                     <div className='flex justify-between items-center'>        
-                        <img src={CDN_URL+ rest.card.info.imageId} className='w-32 '/>
+                        <img src=
+                        {
+                            (rest?.card?.info?.imageId === undefined)
+                            ? noImageAvailable
+                            : CDN_URL+ rest?.card?.info?.imageId
+                            
+                        } 
+
+
+
+                            className='w-32 '/>
                         <div>
+
+                        {
+    console.log(CDN_URL+ rest?.card?.info?.imageId)
+}
+
                             <button className='w-32 h-9 text-slate-600 bg-green-300 rounded-md hover:font-bold'>Add to cart</button>
                         </div>
                     </div>
 
-                        <h1 className='text-lg font-bold leading-4 mb-1'>{rest.card.info.name}</h1>   
+                        <h1 className='text-lg font-bold leading-4 mb-1'>{rest?.card?.info?.name}</h1>   
                         
-                        <h3 className='text-xs font-medium leading-3'>{rest.card.info.description}</h3>
+                        <h3 className='text-xs font-medium leading-3'>{rest?.card?.info?.description}</h3>
 
                       
                         
