edc6d60ddad371247dd720b34686fc506e6e29e8
Aneel Grewal <aneel.grewal@hotmail.com>
2023-06-03 23:08:27 +0200

    accordion created in About component

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,17 @@
-
+import Section from "./Section";
+import {useState} from 'react';
 
 const About = () => {
 
+    const [config, setConfig] = useState("1");
+
     return(
         <div>
-            <h1>About page</h1>
+            <Section isVisible={config === 1} setIsVisible={()=>setConfig("1")} heading="Heading1" para="paragraph1"/>
+
+            <Section isVisible={config === 2} setIsVisible={()=>setConfig("2")} heading="heading2" para="paragraph2"/>
+
+            <Section isVisible={config === 3} setIsVisible={()=>setConfig("3")} heading="heading3" para="paragraph3"/>
         </div>
     )
 }
---
+++ src/components/Section.js
@@ -0,0 +1,29 @@
+
+
+const Section = ({heading, para, isVisible, setIsVisible}) => {
+    return(
+        <div>
+            <div className="m-2 p-2 border border-gray-500">
+                {
+                    isVisible
+                    ?(
+                    <div onClick={setIsVisible} className="px-2 flex justify-between items-center bg-slate-200 cursor-pointer">
+                        <h3 className=" text-lg font-bold ">{heading}</h3>
+                        <p>🔽</p>
+                    </div>
+                    )
+                    :
+                    (<div>
+                    <div onClick={setIsVisible} className="px-2 flex justify-between items-center bg-slate-200 cursor-pointer">
+                        <h3 className="text-lg font-bold">{heading}</h3>
+                        <p>🔼</p>
+                    </div>
+                    <p className="px-2 py-2 text-sm leading-4">{para}</p></div>)
+                }
+                
+            </div>
+        </div>
+    )
+}
+
+export default Section;
