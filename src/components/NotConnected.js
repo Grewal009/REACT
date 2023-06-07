50a59170de3ad1d6b4811b229bc1983c073876ff
Aneel Grewal <aneel.grewal@hotmail.com>
2023-06-07 11:17:41 +0200

    NotConnected component edited

--- src/components/NotConnected.js
+++ src/components/NotConnected.js
@@ -2,9 +2,9 @@
 
 const NotConnected = () => {
     return(
-        <div className="w-full h-[500px] flex justify-center items-center">
+        <div className="w-full h-[200px] sm:h-[300px] flex justify-center items-center">
             <div className="">
-                <h1 className="w-96 p-2 text-lg font-bold text-gray-700 text-center bg-red-200 rounded-3xl">You are not connected to the Internet</h1>
+                <h1 className="w-72 m-auto sm:w-96 p-2 text-sm md:text-lg font-bold text-gray-700 text-center bg-red-200 rounded-3xl">You are not connected to the Internet</h1>
             </div>   
         </div> 
     )
